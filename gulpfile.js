5c784febf2215289cfa381deacd8e5ebaf24f8fe
anothar <anothar@ya.ru>
2016-08-08 12:31:16 +0500

    added webserver task to view demo

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,30 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var minify = require('gulp-minify');
 var concat = require('gulp-concat');
+var webserver = require('gulp-webserver');
 
-gulp.task('compile', function() {
+gulp.task('demo', function () {
+    gulp.src('.')
+        .pipe(webserver({
+            livereload: true,
+            open:'demo/index.html'
+        }));
+});
+
+gulp.task('compile', function () {
     var tsProject = ts.createProject('tsconfig.json');
 
     var tsResult = tsProject.src().pipe(ts(tsProject));
- 
+
     return tsResult.js.pipe(concat('angular-drag-and-drop-lists.js')).pipe(minify({
-        ext:{
-            src:'.js',
-            min:'.min.js'
+        ext: {
+            src: '.js',
+            min: '.min.js'
         },
         ignoreFiles: ['.min.js']
     })).pipe(gulp.dest('release'));
 });
 
-gulp.task('default', ['compile'], function() {
+gulp.task('default', ['compile'], function () {
     return gulp.watch('sources/**/*.ts', ['compile']);
 })
